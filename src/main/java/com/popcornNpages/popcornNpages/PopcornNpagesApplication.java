86992e78ac83a791b5f8778e3b7c4d17e02abdea
AamulSri <aamul24.vns@gmail.com>
2025-06-26 18:39:37 +0530

    Initial commit for PopcornNPages backend

---
+++ src/main/java/com/popcornNpages/popcornNpages/PopcornNpagesApplication.java
@@ -0,0 +1,13 @@
+package com.popcornNpages.popcornNpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornNpagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornNpagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popcornNpages/popcornNpages/PopcornNpagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popcornNpages.popcornNpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornNpagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
